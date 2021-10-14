46ce584355c1c3a069892d9a2c74204fbfe82a5c
mb <ichbinmarkus@gmail.com>
2021-10-14 20:36:54 +0200

    fixes to setup.cfg

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 import setuptools
 import subprocess
 import shutil
+import configparser
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
+with open("README.md", "r", encoding="utf-8") as fh: long_description = fh.read()
 subprocess.call(["pipreqs", ".", "--force"])
 
-setuptools.setup(
+cfg = configparser.ConfigParser()
+cfg.read("setup.cfg")
 
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    install_requires=open("requirements.txt", "r").readlines()
-)
+dct = {}
+for s in cfg.sections():
+    for k,v in cfg[s].items():
+        dct[k] = v
 
+dct["long_description"] = long_description
+dct["long_description_content_type"] = "text/markdown"
+dct["package_dir"] = {"": "src"}
+dct["packages"] = setuptools.find_packages(where="src")
+dct["install_requires"] = open("requirements.txt", "r").readlines()
+
+setuptools.setup(**dct)
 shutil.rmtree("build", ignore_errors=True)
 shutil.rmtree("src/dfana.egg-info", ignore_errors=True)
