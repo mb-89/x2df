4f9dbe080388b4c89d87da196e1bfedf805bb596
mb <ichbinmarkus@gmail.com>
2021-10-15 12:28:30 +0200

    added tests

--- src/x2df/x2df.py
+++ src/x2df/x2df.py
@@ -9,18 +9,24 @@ def main(argv):
     parser = argparse.ArgumentParser(
         "parses given glob-style paths and extracts dataframes"
     )
-    parser.add_argument("paths", nargs="*")
+    parser.add_argument("srcs", nargs="*", help="glob-style paths that will be parsed")
+    parser.add_argument("dst", nargs="?", help="destination path for the results")
     parser.add_argument(
         "-?", action="store_true", help="show this help message and exit"
     )
     parser.add_argument("-v", "--version", action="store_true", help="prints version")
     args = argv[1:]
     args = vars(parser.parse_args(args))
+    if len(args["srcs"]) > 1:
+        args["dst"] = args["srcs"].pop(-1)
+
     if args["version"]:
         print(__version__)
-        return
-    if args["?"] or not args["paths"]:
+        return 0
+    if args["?"] or not args["srcs"] or not args["dst"]:
         parser.print_help()
         return 0
+
+    # if we are here, we have srcs and a dst
     log.info(args)
     return 0
--- tests/test_fundamentals.py
+++ tests/test_fundamentals.py
@@ -5,7 +5,9 @@ from x2df import logfuns
 def test_main():
     assert logfuns.setupLogging() == 0  # first pass for logging setup
     assert logfuns.setupLogging() == -1  # second pass: skipped bc already set up
-    assert x2df.main(["test"]) == 0
+    assert x2df.main(["test", "-v"]) == 0
+    assert x2df.main(["test", "-?"]) == 0
+    assert x2df.main(["test", "src", "dst"]) == 0
 
 
 if __name__ == "__main__":
